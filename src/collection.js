42e0f8ca276e1fb4f0c931897c7ffb1a37f4b97e
confused-Techie <dev@lhbasics.com>
2022-06-24 18:49:31 -0700

    Most sort methods written, attempt fix of eslint config

--- src/collection.js
+++ src/collection.js
@@ -3,6 +3,7 @@
 
 /**
  * @desc Intended for use for a collection of Packages, sort them according to any valid Sorting method.
+ * Note this should be called before, any Pruning has taken place.
  * @async
  * @function Sort
  * @param {string} method - The Method to Sort By
@@ -10,18 +11,75 @@
  * @return {object} The provided packages now sorted accordingly.
  */
 async function Sort(packages, method) {
+  // Note Sort, should be called before ANY pruning action has taken place.
+  // Additional note, this implementation will sort EVERY single package no matter what.
+  // TODO: Feature Request: Provide the page requested during sort, or the last item needed to sort,
+  // then discard the rest of the array. Pruning length of array may actually be a smart move for all additional functions.
   if (method == "downloads") {
+
+    packages.sort((a, b) => {
+      if (a.downloads < b.downloads) {
+        return 1;
+      }
+      if (a.downloads > b.downloads) {
+        return -1;
+      }
+      return 0;
+    });
+
     return packages;
+
   } else if (method == "created_at") {
+
+    packages.sort((a, b) => {
+      if (a.created < b.created) {
+        return 1;
+      }
+      if (a.created > b.created) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return packages;
+
   } else if (method == "updated_at") {
+
+    packages.sort((a, b) => {
+      if (a.updated < b.updated) {
+        return 1;
+      }
+      if (a.updated > b.updated) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return packages;
+
   } else if (method == "stars") {
+
+    packages.sort((a, b) => {
+      if (a.stargazers_count < b.stargazers_count) {
+        return 1;
+      }
+      if (a.stargazers_count > b.stargazers_count) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return packages;
+
   } else if (method == "relevance") {
+    // TODO search method to then find parameter of relevance.
   } else {
     return "Unrecognized Sorting Method!";
   }
 }
 
 async function Direction(packages, method) {
+  // While previously
   if (method == "desc") {
     // since we wrote the sort, we know it will return results, sorted by the default of desc, and we can return.
     return packages;
@@ -48,22 +106,23 @@ async function Prune(packages) {
   // star_gazers (A list of all star_gazers users), updated, created,
 
   // Prune may also encounter an array of items, or a single item.
+  // TODO: TODO: This is based on an old schema model, and is now improper.
   if (Array.isArray(packages)) {
     for (var i = 0; i < packages.length; i++) {
       // Reference below non-array argument for each removed value.
-      delete packages[i].star_gazers;
-      delete packages[i].updated;
-      delete packages[i].created;
+      //delete packages[i].star_gazers;
+      //delete packages[i].updated;
+      //delete packages[i].created;
     }
 
     return packages;
   } else {
     // Remove star_gazers
-    delete packages.star_gazers;
+    //delete packages.star_gazers;
     // Remove updated
-    delete packages.updated;
+    //delete packages.updated;
     // Remove created
-    delete packages.created;
+    //delete packages.created;
     //Return the package
     return packages;
   }
