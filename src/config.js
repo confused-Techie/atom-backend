f023193fa98196525a84f22e76e6c604b5c660ec
confused-Techie <dev@lhbasics.com>
2022-07-06 18:18:50 -0700

    Test again for config in runner

--- src/config.js
+++ src/config.js
@@ -22,7 +22,7 @@ function GetConfig() {
       data = yaml.load(fileContent);
     } catch (err) {
       // We failed to get the config file. But if we have env vars in production its okay.
-      if (!process.env.NODE_ENV == "production") {
+      if (!process.env.NODE_ENV == "production" && !process.env.NODE_ENV == "test") {
         console.log(`Failed to load app.yaml in non-production env! ${err}`);
         process.exit(1);
       } // else we should be okay.
