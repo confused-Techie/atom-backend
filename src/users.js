bdf407d50c492e30903ff3aced5fd2232e0919d8
Digitalone <kurmikon@libero.it>
2022-07-24 09:05:06 +0200

    code refactoring

--- src/users.js
+++ src/users.js
@@ -20,20 +20,20 @@ const data = require("./data.js");
  */
 async function VerifyAuth(token) {
   const users = await data.GetUsers();
-  if (users.ok) {
-    for (const user in users.content) {
-      const usrToken = users.content[user].atom_token;
 
-      if (usrToken !== undefined) {
-        if (usrToken == token) {
-          return { ok: true, content: users.content[user] };
-        }
-      }
-    }
-    return { ok: false, content: "No valid token found.", short: "Bad Auth" };
-  } else {
+  if (!users.ok) {
     return users;
   }
+
+  for (const user in users.content) {
+    const usrToken = users.content[user].atom_token;
+
+    if (usrToken !== undefined && usrToken == token) {
+      return { ok: true, content: users.content[user] };
+    }
+  }
+
+  return { ok: false, content: "No valid token found.", short: "Bad Auth" };
 }
 
 /**
@@ -47,16 +47,17 @@ async function VerifyAuth(token) {
  */
 async function GetUser(username) {
   const users = await data.GetUsers();
-  if (users.ok) {
-    if (users.content[username]) {
-      // user has been found, and we have a user object available.
-      return { ok: true, content: users.content[username] };
-    } else {
-      // user isn't found.
-      return { ok: false, content: "Not Found", short: "Not Found" };
-    }
-  } else {
+
+  if (!users.ok) {
     return users;
+  }
+
+  if (users.content[username]) {
+    // user has been found, and we have a user object available.
+    return { ok: true, content: users.content[username] };
+  } else {
+    // user isn't found.
+    return { ok: false, content: "Not Found", short: "Not Found" };
   }
 }
 
@@ -75,31 +76,28 @@ async function AddUserStar(packageName, userName) {
   // this lets us add the packageName to the users list of stars.
   let user = await GetUser(userName);
 
-  if (user.ok) {
-    // with the user, lets add the package
-    user.content.stars.push(packageName);
-    // then write the user data.
-
-    // A bug discovered is this writes the user data singular object, not the entire user file object.
-    let users = await data.GetUsers();
-    if (users.ok) {
-      users.content[userName] = user.content;
-
-      // now with the new user object assigned as the key to the user file, we can save.
-      const write = data.SetUsers(users.content);
-
-      if (write.ok) {
-        return { ok: true };
-      } else {
-        return write;
-      }
-    } else {
-      //unable to read file
-      return users;
-    }
-  } else {
+  if (!user.ok) {
     return user;
   }
+
+  // with the user, lets add the package
+  user.content.stars.push(packageName);
+  // then write the user data.
+
+  // A bug discovered is this writes the user data singular object, not the entire user file object.
+  let users = await data.GetUsers();
+
+  if (!users.ok) {
+    //unable to read file
+    return users;
+  }
+
+  users.content[userName] = user.content;
+
+  // now with the new user object assigned as the key to the user file, we can save.
+  const write = data.SetUsers(users.content);
+
+  return write.ok ? { ok: true } : write;
 }
 
 /**
@@ -116,40 +114,39 @@ async function AddUserStar(packageName, userName) {
 async function RemoveUserStar(packageName, userName) {
   let user = await GetUser(userName);
 
-  if (user.ok) {
-    // find the index of the star in the users stars array
-    let starIdx = -1;
-    for (let i = 0; i < user.content.stars.length; i++) {
-      if (user.content.stars[i] === packageName) {
-        starIdx = i;
-        break;
-      }
-    }
-    if (starIdx !== -1) {
-      // now to remove
-      user.content.stars.splice(starIdx, 1);
-
-      // then to write the new user data
-      // Same bug as AddUserStar
-      let users = await data.GetUsers();
-      if (users.ok) {
-        users.content[userName] = user.content;
-        const write = data.SetUsers(users.content);
-        if (write.ok) {
-          return { ok: true };
-        } else {
-          return write;
-        }
-      } else {
-        return users;
-      }
-    } else {
-      // failed to find the star in the user star array
-      return { ok: false, content: "Not Found", short: "Not Found" };
-    }
-  } else {
+  if (!user.ok) {
     return user;
   }
+
+  // find the index of the star in the users stars array
+  let starIdx = -1;
+  for (let i = 0; i < user.content.stars.length; i++) {
+    if (user.content.stars[i] === packageName) {
+      starIdx = i;
+      break;
+    }
+  }
+
+  if (starIdx === -1) {
+    // failed to find the star in the user star array
+    return { ok: false, content: "Not Found", short: "Not Found" };
+  }
+
+  // now to remove
+  user.content.stars.splice(starIdx, 1);
+
+  // then to write the new user data
+  // Same bug as AddUserStar
+  let users = await data.GetUsers();
+
+  if (!users.ok) {
+    return users;
+  }
+
+  users.content[userName] = user.content;
+  const write = data.SetUsers(users.content);
+
+  return write.ok ? { ok: true } : write;
 }
 
 /**
