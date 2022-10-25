c0179c70934b99d5d281184ea061243394999304
confused-Techie <dev@lhbasics.com>
2022-10-24 19:55:00 -0700

    Fixed Bad Naming Convention

--- src/tests/dev-runner.storage.test.js
+++ src/tests/dev-runner.storage.test.js
@@ -3,18 +3,18 @@ const storage = require("../dev-runner/storage.js");
 describe("Functions Return Proper Values", () => {
 
   test("getBanList Dev Returns Array", async () => {
-    let return = storage.getBanList();
-    expect(Array.isArray(return)).toBeTruthy();
+    let value = storage.getBanList();
+    expect(Array.isArray(value)).toBeTruthy();
   });
 
   test("getFeaturedPackages Dev Returns Array", async () => {
-    let return = storage.getFeaturedPackages();
-    expect(Array.isArray(return)).toBeTruthy();
+    let value = storage.getFeaturedPackages();
+    expect(Array.isArray(value)).toBeTruthy();
   });
 
   test("getFeaturedThemes Dev Returns Array", async () => {
-    let return = storage.getFeaturedThemes();
-    expect(Array.isArray(return)).toBeTruthy();
+    let value = storage.getFeaturedThemes();
+    expect(Array.isArray(value)).toBeTruthy();
   });
 
 });
--- src/tests/storage.test.js
+++ src/tests/storage.test.js
@@ -3,18 +3,18 @@ const storage = require("../storage.js");
 describe("Functions Return Proper Values", () => {
 
   test("getBanList Returns Array", async () => {
-    let return = storage.getBanList();
-    expect(Array.isArray(return)).toBeTruthy();
+    let value = storage.getBanList();
+    expect(Array.isArray(value)).toBeTruthy();
   });
 
   test("getFeaturedPackages Returns Array", async () => {
-    let return = storage.getFeaturedPackages();
-    expect(Array.isArray(return)).toBeTruthy();
+    let value = storage.getFeaturedPackages();
+    expect(Array.isArray(value)).toBeTruthy();
   });
 
   test("getFeaturedThemes Returns Array", async () => {
-    let return = storage.getFeaturedThemes();
-    expect(Array.isArray(return)).toBeTruthy();
+    let value = storage.getFeaturedThemes();
+    expect(Array.isArray(value)).toBeTruthy();
   });
 
 });
