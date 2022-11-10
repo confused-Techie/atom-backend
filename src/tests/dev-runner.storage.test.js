7811c3bb758f74d5c4f6d9376d1a43635b8fc3d3
confused-Techie <dev@lhbasics.com>
2022-11-09 21:54:21 -0800

    Apply same fixes to `dev-runner.storage.test.js`

--- src/tests/dev-runner.storage.test.js
+++ src/tests/dev-runner.storage.test.js
@@ -3,16 +3,16 @@ const storage = require("../dev-runner/storage.js");
 describe("Functions Return Proper Values", () => {
   test("getBanList Dev Returns Array", async () => {
     let value = await storage.getBanList();
-    expect(Array.isArray(value)).toBeTruthy();
+    expect(Array.isArray(value.content)).toBeTruthy();
   });
 
   test("getFeaturedPackages Dev Returns Array", async () => {
     let value = await storage.getFeaturedPackages();
-    expect(Array.isArray(value)).toBeTruthy();
+    expect(Array.isArray(value.content)).toBeTruthy();
   });
 
   test("getFeaturedThemes Dev Returns Array", async () => {
     let value = await storage.getFeaturedThemes();
-    expect(Array.isArray(value)).toBeTruthy();
+    expect(Array.isArray(value.content)).toBeTruthy();
   });
 });
