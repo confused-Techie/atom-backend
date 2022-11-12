0e3b3e89a79265897ebe67f0a448a43dcf39b4c0
confused-Techie <confused-Techie@users.noreply.github.com>
2022-11-12 04:38:38 +0000

    GH Action Standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-      "eslint:recommended",
-      "plugin:node/recommended"
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:node/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "node/no-unpublished-require": [
+      "error",
+      {
+        allowModules: ["supertest"],
+      },
     ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-      "node/no-unpublished-require": ["error", {
-        "allowModules": [
-          "supertest"
-        ]
-      }]
-    },
-    "plugins": [
-      "node/recommended"
-    ],
-    "globals": {
-      "jest": "readonly",
-      "test": "readonly",
-      "expect": "readonly",
-      "describe": "readonly",
-      "beforeAll": "readonly",
-      "afterEach": "readonly",
-      "process": "writeable",
-      "Buffer": "readonly"
-    }
-}
+  },
+  plugins: ["node/recommended"],
+  globals: {
+    jest: "readonly",
+    test: "readonly",
+    expect: "readonly",
+    describe: "readonly",
+    beforeAll: "readonly",
+    afterEach: "readonly",
+    process: "writeable",
+    Buffer: "readonly",
+  },
+};
