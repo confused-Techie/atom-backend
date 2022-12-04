f67db7b9ddcdad50efd2565ec31c8e40632b50a2
confused-Techie <confused-Techie@users.noreply.github.com>
2022-12-04 07:54:40 +0000

    GH Action Standards

--- src/handlers/package_handler.js
+++ src/handlers/package_handler.js
@@ -63,11 +63,11 @@ async function getPackages(req, res) {
       params.sort
     }&order=${
       params.direction
-    }>; rel="self", <${server_url}/api/packages?page=${totalPages.content}&sort=${
-      params.sort
-    }&order=${
+    }>; rel="self", <${server_url}/api/packages?page=${
+      totalPages.content
+    }&sort=${params.sort}&order=${
       params.direction
-    }>; rel="last", <${server_url}/api/packages?page=${params.page+1}&sort=${
+    }>; rel="last", <${server_url}/api/packages?page=${params.page + 1}&sort=${
       params.sort
     }&order=${params.direction}>; rel="next"`
   );
@@ -276,9 +276,9 @@ async function getPackagesSearch(req, res) {
       params.page
     }&sort=${params.sort}&order=${
       params.direction
-    }>; rel="self", <${server_url}/api/packages?q=${
-      params.query
-    }&page=${totalPages.content}&sort=${params.sort}&order=${
+    }>; rel="self", <${server_url}/api/packages?q=${params.query}&page=${
+      totalPages.content
+    }&sort=${params.sort}&order=${
       params.direction
     }>; rel="last", <${server_url}/api/packages/search?q=${params.query}&page=${
       params.page + 1
