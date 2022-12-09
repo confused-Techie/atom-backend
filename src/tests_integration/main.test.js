ac2801b03ca354edd41e603e10f2ef3d12b62a40
confused-Techie <dev@lhbasics.com>
2022-12-08 21:11:40 -0800

    Even more tests

--- src/tests_integration/main.test.js
+++ src/tests_integration/main.test.js
@@ -102,6 +102,14 @@ describe("Get /api/login", () => {
 });
 
 describe("Get /api/oauth", () => {
+  test("Returns Not Found Status Code when provided no State", async () => {
+    const res = await request(app).get("/api/oauth");
+    expect(res).toHaveHTTPCode(404);
+  });
+  test("Returns Not Found Message when provided no State", async () => {
+    const res = await request(app).get("/api/oauth");
+    expect(res.body.message).toEqual(msg.notFound);
+  });
   test.todo(
     "Can we test? This section has been tested manually. But beyond that seems impossible to truly test."
   );
@@ -490,6 +498,14 @@ describe("GET /api/packages/:packageName/stargazers", () => {
 });
 
 describe("POST /api/packages/:packageName/versions", () => {
+  test("Returns 404 with Bad Package", async () => {
+    const res = await request(app).post("/api/packages/language-golang/versions");
+    expect(res).toHaveHTTPCode(401);
+  });
+  test("Returns Bad Auth Message with No Auth", async () => {
+    const res = await request(app).post("/api/packages/language-golang/versions");
+    expect(res.body.message).toEqual(msg.badAuth);
+  });
   test.todo("Write all tests on this endpoint");
 });
 
