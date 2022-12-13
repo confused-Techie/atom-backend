479c465b4efbf7e70681abc28ee23d2dcc3868e2
confused-Techie <dev@lhbasics.com>
2022-12-12 19:20:22 -0800

    Added new Statements used in Production supporting `theme` distinction

--- scripts/database/create_packages_table.sql
+++ scripts/database/create_packages_table.sql
@@ -19,3 +19,19 @@ CREATE TABLE packages (
 -- https://github.com/confused-Techie/atom-backend/issues/90
 
 ALTER TABLE packages ADD CONSTRAINT lowercase_names CHECK (name = LOWER(name));
+
+ALTER TABLE packages ADD COLUMN package_type VARCHAR(20);
+
+UPDATE packages
+SET package_type = 'theme'
+WHERE LOWER(data ->'metadata'->> 'theme') = 'syntax' OR LOWER(data ->'metadata'->>'theme') = 'ui';
+
+UPDATE packages
+SET package_type = 'package'
+WHERE package_type IS DISTINCT FROM 'theme';
+
+CREATE TYPE packageType AS ENUM('package', 'theme');
+
+ALTER TABLE packages
+ALTER COLUMN package_type TYPE packageType USING (package_type::packageType),
+ALTER COLUMN package_type SET NOT NULL;
